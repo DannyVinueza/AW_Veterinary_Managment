1111ae142576eff7cc8d8569da44dfb555e69b6d
DAQG <daniel.quishpe@epn.edu.ec>
2023-08-28 15:46:14 -0500

    feat: formik en primer formulario

--- src/componets/Perfil/FormularioPerfil.jsx
+++ src/componets/Perfil/FormularioPerfil.jsx
@@ -1,20 +1,38 @@
-import { useContext, useState } from "react"
-import AuthContext from "../../context/AuthProvider"
-import Mensaje from "../Alertas/Mensaje"
-
+import { useContext, useState } from "react";
+import AuthContext from "../../context/AuthProvider";
+import Mensaje from "../Alertas/Mensaje";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 const FormularioPerfil = () => {
-    const [mensaje, setMensaje] = useState({})
-    const {auth,actualizarPerfil} = useContext(AuthContext)
-    const [form, setform] = useState({
-		id: auth._id,
-        nombre: auth.nombre || "",
-        apellido:auth.apellido || "",
-        direccion: auth.direccion || "",
-        telefono: auth.telefono || "",
-        email: auth.email || ""
-    })
+  const [mensaje, setMensaje] = useState({});
+  const { auth, actualizarPerfil } = useContext(AuthContext);
 
+  const formik = useFormik({
+    initialValues: {
+      nombre: auth.nombre || "",
+      apellido: auth.apellido || "",
+      direccion: auth.direccion || "",
+      telefono: auth.telefono || "",
+      email: auth.email || "",
+    },
+    validationSchema: Yup.object({
+      nombre: Yup.string().required("El nombre es requerido"),
+      apellido: Yup.string().required("El apellido es requerido"),
+      direccion: Yup.string().required("La dirección es requerida"),
+      telefono: Yup.string().required("El teléfono es requerido"),
+      email: Yup.string()
+        .email("Correo electrónico no válido")
+        .required("El correo electrónico es requerido"),
+    }),
+    onSubmit: async (values) => {
+      const resultado = await actualizarPerfil(values);
+      setMensaje(resultado);
+      setTimeout(() => {
+        setMensaje({});
+      }, 3000);
+    },
+  });
     const handleChange = (e) => {
         setform({
             ...form,
@@ -39,94 +57,159 @@ const FormularioPerfil = () => {
         }, 3000);
   }
     
-    
-    return (
-        <form onSubmit={handleSubmit} >
-             {Object.keys(mensaje).length > 0 && <Mensaje tipo={mensaje.tipo}>{mensaje.respuesta}</Mensaje>}
-            <div>
-                <label
-                    htmlFor='nombre'
-                    className='text-gray-700 uppercase font-bold text-sm'>Nombre: </label>
-                <input
-                    id='nombre'
-                    type="text"
-                    className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
-                    placeholder='nombre'
-                    name='nombre'
-                    value={form.nombre}
-                    onChange={handleChange}
-                />
-            </div>
+   
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      {Object.keys(mensaje).length > 0 && (
+        <Mensaje tipo={mensaje.tipo}>{mensaje.respuesta}</Mensaje>
+      )}
+      <div className="mb-5">
+        <label
+          htmlFor="nombre"
+          className="text-gray-700 uppercase font-bold text-sm"
+        >
+          Nombre:
+        </label>
+        <input
+          id="nombre"
+          type="text"
+          className={`border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md  ${
+            formik.touched.nombre && formik.errors.nombre
+              ? "border-red-500"
+              : "border-gray-300"
+          }`}
+          placeholder="nombre"
+          name="nombre"
+          value={formik.values.nombre}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+        {formik.touched.nombre && formik.errors.nombre ? (
+          <div className="text-red-500 text-sm">
+            {formik.errors.nombre}
+          </div>
+        ) : null}
+      </div>
 
-            <div>
-                <label
-                    htmlFor='apellido'
-                    className='text-gray-700 uppercase font-bold text-sm'>Apellido: </label>
-                <input
-                    id='apellido'
-                    type="text"
-                    className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
-                    placeholder='apellido'
-                    name='apellido'
-                    value={form.apellido}
-                    onChange={handleChange}
-                />
-            </div>
+      <div className="mb-5" >
+        <label
+          htmlFor="apellido"
+          className="text-gray-700 uppercase font-bold text-sm"
+        >
+          Apellido:
+        </label>
+        <input
+          id="apellido"
+          type="text"
+          className={`border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md  ${
+            formik.touched.apellido && formik.errors.apellido
+              ? "border-red-500"
+              : "border-gray-300"
+          }`}
+          placeholder="apellido"
+          name="apellido"
+          value={formik.values.apellido}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+        {formik.touched.apellido && formik.errors.apellido ? (
+          <div className="text-red-500 text-sm">
+            {formik.errors.apellido}
+          </div>
+        ) : null}
+      </div>
 
-            <div>
-                <label
-                    htmlFor='direccion'
-                    className='text-gray-700 uppercase font-bold text-sm'>Dirección: </label>
-                <input
-                    id='direccion'
-                    type="text"
-                    className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
-                    placeholder='direccion'
-                    name='direccion'
-                    value={form.direccion}
-                    onChange={handleChange}
-                />
-            </div>
+      <div className="mb-5">
+        <label
+          htmlFor="direccion"
+          className="text-gray-700 uppercase font-bold text-sm"
+        >
+          Dirección:
+        </label>
+        <input
+          id="direccion"
+          type="text"
+          className={`border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md  ${
+            formik.touched.direccion && formik.errors.direccion
+              ? "border-red-500"
+              : "border-gray-300"
+          }`}
+          placeholder="direccion"
+          name="direccion"
+          value={formik.values.direccion}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+        {formik.touched.direccion && formik.errors.direccion ? (
+          <div className="text-red-500 text-sm">
+            {formik.errors.direccion}
+          </div>
+        ) : null}
+      </div>
 
-            <div>
-                <label
-                    htmlFor='telefono'
-                    className='text-gray-700 uppercase font-bold text-sm'>Teléfono: </label>
-                <input
-                    id='ditelefonoreccion'
-                    type="text"
-                    className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
-                    placeholder='telefono'
-                    name='telefono'
-                    value={form.telefono}
-                    onChange={handleChange}
-                />
-            </div>
+      <div className="mb-5">
+        <label
+          htmlFor="telefono"
+          className="text-gray-700 uppercase font-bold text-sm"
+        >
+          Teléfono:
+        </label>
+        <input
+          id="telefono"
+          type="text"
+          className={`border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md ${
+            formik.touched.telefono && formik.errors.telefono
+              ? "border-red-500"
+              : "border-gray-300"
+          }`}
+          placeholder="telefono"
+          name="telefono"
+          value={formik.values.telefono}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+        {formik.touched.telefono && formik.errors.telefono ? (
+          <div className="text-red-500 text-sm">
+            {formik.errors.telefono}
+          </div>
+        ) : null}
+      </div>
 
-            <div>
-                <label
-                    htmlFor='email'
-                    className='text-gray-700 uppercase font-bold text-sm'>Email: </label>
-                <input
-                    id='email'
-                    type="text"
-                    className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
-                    placeholder='email'
-                    name='email'
-                    value={form.email}
-                    onChange={handleChange}
-                />
-            </div>
+      <div className="mb-5">
+        <label
+          htmlFor="email"
+          className="text-gray-700 uppercase font-bold text-sm"
+        >
+          Email:
+        </label>
+        <input
+          id="email"
+          type="text"
+          className={`border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md  ${
+            formik.touched.email && formik.errors.email
+              ? "border-red-500"
+              : "border-gray-300"
+          }`}
+          placeholder="email"
+          name="email"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+        {formik.touched.email && formik.errors.email ? (
+          <div className="text-red-500 text-sm">{formik.errors.email}</div>
+        ) : null}
+      </div>
 
-            <input
-                type="submit"
-                className='bg-gray-800 w-full p-3 
+      <input
+        type="submit"
+        className="bg-gray-800 w-full p-3 
         text-slate-300 uppercase font-bold rounded-lg 
-        hover:bg-gray-600 cursor-pointer transition-all'
-                value='Actualizar' />
+        hover:bg-gray-600 cursor-pointer transition-all"
+        value="Actualizar"
+      />
+    </form>
+  );
+};
 
-        </form>
-    )
-}
-
-export default FormularioPerfil
+export default FormularioPerfil;
