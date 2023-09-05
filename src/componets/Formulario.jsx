b1081f75573dc8be02a769ef5d46297c1e38a0f7
DannyVinueza <danny.vinueza@epn.edu.ec>
2023-09-05 00:19:41 -0500

    Omision por el moemneto de validación de inputs

--- src/componets/Formulario.jsx
+++ src/componets/Formulario.jsx
@@ -1,212 +1,179 @@
-import { useContext, useState } from "react";
-import { useNavigate } from 'react-router-dom';
-import AuthContext from "../context/AuthProvider";
+import { useContext, useState } from "react"
+import { useNavigate } from 'react-router-dom'
+import AuthContext from "../context/AuthProvider"
 import axios from 'axios';
 import Mensaje from "./Alertas/Mensaje";
-import { useFormik } from "formik";
-import * as Yup from "yup";
 
-export const Formulario = ({ paciente }) => {
-  const { auth } = useContext(AuthContext);
-  const navigate = useNavigate();
-  const [mensaje, setMensaje] = useState({});
-  const nombreValido = (value) => {
-    const regex = /^[A-Za-z]+$/; // Expresión regular que permite solo letras
-    return regex.test(value) || "El nombre de la mascota solo debe contener letras";
-  };
 
-  // Define el esquema de validación utilizando Yup
-  const validationSchema = Yup.object().shape({
-        nombre: Yup.string()
-      .required("El nombre de la mascota es obligatorio")
-      .test("nombreValido", "El nombre de la mascota solo debe contener letras", nombreValido),
-    propietario: Yup.string().required("El nombre del propietario es obligatorio"),
-    email: Yup.string().email("Correo electrónico no válido").required("El correo electrónico es obligatorio"),
-    celular: Yup.number().required("El número de celular es obligatorio"),
-    convencional: Yup.number().required("El número convencional es obligatorio"),
-    salida: Yup.date().required("La fecha de salida es obligatoria"),
-    sintomas: Yup.string().required("Los síntomas son obligatorios"),
-  });
+export const Formulario = ( {paciente}) => {
 
-  // Define los valores iniciales del formulario
-  const initialValues = {
-    nombre: paciente?.nombre || "",
-    propietario: paciente?.propietario || "",
-    email: paciente?.email || "",
-    celular: paciente?.celular || "",
-    convencional: paciente?.convencional || "",
-    salida: paciente?.salida ? new Date(paciente.salida).toLocaleDateString("en-CA", { timeZone: "UTC" }) : "",
-    sintomas: paciente?.sintomas || "",
-  };
+    const {auth} = useContext(AuthContext)
+    const navigate = useNavigate()
+    const [mensaje, setMensaje] = useState({})
+    const [form, setform] = useState({
+        nombre: paciente?.nombre ??"",
+        propietario: paciente?.propietario ??"",
+        email: paciente?.email ??"",
+        celular: paciente?.celular ??"",
+        salida:  new Date(paciente?.salida).toLocaleDateString('en-CA', {timeZone: 'UTC'}) ?? "",
+        convencional: paciente?.convencional ??"",
+        sintomas: paciente?.sintomas ??""
+    })
 
-  // Hook de Formik
-  const formik = useFormik({
-    initialValues,
-    validationSchema,
-    onSubmit: async (values) => {
-      try {
-        const token = localStorage.getItem('token');
-        values.id = auth._id;
-        const url = `${import.meta.env.VITE_BACKEND_URL}/paciente/registro`;
-        const options = {
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${token}`,
-          },
-        };
-        await axios.post(url, values, options);
-        navigate('/dashboard/listar');
-      } catch (error) {
-        setMensaje({ respuesta: error.response?.data.msg, tipo: false });
+    const handleChange = (e) => {
+        setform({...form,
+            [e.target.name]:e.target.value
+        })
+    }
+
+    const handleSubmit = async(e) => { 
+        e.preventDefault()
+        if (paciente?._id) {
+            const token = localStorage.getItem('token')
+            const url = `${import.meta.env.VITE_BACKEND_URL}/paciente/actualizar/${paciente?._id}`
+            const options = {
+                headers: {
+                    method: 'PUT',
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`
+                }
+            }
+            await axios.put(url, form, options)
+            navigate('/dashboard/listar')
+        }else{
+        
+    }
+
+    try {
+        const token = localStorage.getItem('token')
+        // form.id = auth._id
+        const url = `${import.meta.env.VITE_BACKEND_URL}/paciente/registro`
+        const options={
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`
+            }
+        }
+        await axios.post(url,form,options)
+        navigate('/dashboard/listar')
+    } catch (error) {
+                    setMensaje({ respuesta: error.response?.data.msg, tipo: false })
         setTimeout(() => {
-          setMensaje({});
+            setMensaje({})
         }, 3000);
-      }
-    },
-  });
+    }
+}
 
-  return (
-    <form onSubmit={formik.handleSubmit}>
-      {Object.keys(mensaje).length > 0 && <Mensaje tipo={mensaje.tipo}>{mensaje.respuesta}</Mensaje>}
-      
-      <div className="mb-5">
-        <label htmlFor="nombre" className="text-gray-700 uppercase font-bold text-sm">
-          Nombre de la mascota:
-        </label>
-        <input
-          id="nombre"
-          type="text"
-          className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md "
-          placeholder="nombre de la mascota"
-          name="nombre"
-          value={formik.values.nombre}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-        />
-        {formik.touched.nombre && formik.errors.nombre && (
-          <div className="text-red-500 text-sm">{formik.errors.nombre}</div>
-        )}
-      </div>
-      
-      <div className="mb-5">
-        <label htmlFor="propietario" className="text-gray-700 uppercase font-bold text-sm">
-          Nombre del propietario:
-        </label>
-        <input
-          id="propietario"
-          type="text"
-          className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md "
-          placeholder="nombre del propietario"
-          name="propietario"
-          value={formik.values.propietario}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-        />
-        {formik.touched.propietario && formik.errors.propietario && (
-          <div className="text-red-500 text-sm">{formik.errors.propietario}</div>
-        )}
-      </div>
-      
-      <div className="mb-5">
-        <label htmlFor="email" className="text-gray-700 uppercase font-bold text-sm">
-          Email:
-        </label>
-        <input
-          id="email"
-          type="email"
-          className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md "
-          placeholder="email del propietario"
-          name="email"
-          value={formik.values.email}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-        />
-        {formik.touched.email && formik.errors.email && (
-          <div className="text-red-500 text-sm">{formik.errors.email}</div>
-        )}
-      </div>
-      
-      <div className="mb-5">
-        <label htmlFor="celular" className="text-gray-700 uppercase font-bold text-sm">
-          Celular:
-        </label>
-        <input
-          id="celular"
-          type="number"
-          className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md "
-          placeholder="celular del propietario"
-          name="celular"
-          value={formik.values.celular}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-        />
-        {formik.touched.celular && formik.errors.celular && (
-          <div className="text-red-500 text-sm">{formik.errors.celular}</div>
-        )}
-      </div>
-      
-      <div className="mb-5">
-        <label htmlFor="convencional" className="text-gray-700 uppercase font-bold text-sm">
-          Convencional:
-        </label>
-        <input
-          id="convencional"
-          type="number"
-          className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md "
-          placeholder="convencional del propietario"
-          name="convencional"
-          value={formik.values.convencional}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-        />
-        {formik.touched.convencional && formik.errors.convencional && (
-          <div className="text-red-500 text-sm">{formik.errors.convencional}</div>
-        )}
-      </div>
-      
-      <div className="mb-5">
-        <label htmlFor="salida" className="text-gray-700 uppercase font-bold text-sm">
-          Fecha de salida:
-        </label>
-        <input
-          id="salida"
-          type="date"
-          className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md "
-          placeholder="salida"
-          name="salida"
-          value={formik.values.salida}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-        />
-        {formik.touched.salida && formik.errors.salida && (
-          <div className="text-red-500 text-sm">{formik.errors.salida}</div>
-        )}
-      </div>
-      
-      <div className="mb-5">
-        <label htmlFor="sintomas" className="text-gray-700 uppercase font-bold text-sm">
-          Síntomas:
-        </label>
-        <textarea
-          id="sintomas"
-          type="text"
-          className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md "
-          placeholder="Ingrese los síntomas de la mascota"
-          name="sintomas"
-          value={formik.values.sintomas}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-        />
-        {formik.touched.sintomas && formik.errors.sintomas && (
-          <div className="text-red-500 text-sm">{formik.errors.sintomas}</div>
-        )}
-      </div>
+    return (
+        
+        <form onSubmit={handleSubmit}>
+            {Object.keys(mensaje).length>0 && <Mensaje tipo={mensaje.tipo}>{mensaje.respuesta}</Mensaje>}
+            <div>
+                <label
+                    htmlFor='nombre:'
+                    className='text-gray-700 uppercase font-bold text-sm'>Nombre de la mascota: </label>
+                <input
+                    id='nombre'
+                    type="text"
+                    className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
+                    placeholder='nombre de la mascota'
+                    name='nombre'
+                    value={form.nombre}
+                    onChange={handleChange}
+                />
+            </div>
+            <div>
+                <label
+                    htmlFor='propietario:'
+                    className='text-gray-700 uppercase font-bold text-sm'>Nombre del propietario: </label>
+                <input
+                    id='propietario'
+                    type="text"
+                    className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
+                    placeholder='nombre del propietario'
+                    name='propietario'
+                    value={form.propietario}
+                    onChange={handleChange}
+                />
+            </div>
+            <div>
+                <label
+                    htmlFor='email:'
+                    className='text-gray-700 uppercase font-bold text-sm'>Email: </label>
+                <input
+                    id='email'
+                    type="email"
+                    className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
+                    placeholder='email del propietario'
+                    name='email'
+                    value={form.email}
+                    onChange={handleChange}
+                />
+            </div>
+            <div>
+                <label
+                    htmlFor='celular:'
+                    className='text-gray-700 uppercase font-bold text-sm'>Celular: </label>
+                <input
+                    id='celular'
+                    type="number"
+                    className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
+                    placeholder='celular del propietario'
+                    name='celular'
+                    value={form.celular}
+                    onChange={handleChange}
+                />
+            </div>
+            <div>
+                <label
+                    htmlFor='convencional:'
+                    className='text-gray-700 uppercase font-bold text-sm'>Convencional: </label>
+                <input
+                    id='convencional'
+                    type="number"
+                    className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
+                    placeholder='convencional del propietario'
+                    name='convencional'
+                    value={form.convencional}
+                    onChange={handleChange}
+                />
+            </div>
+            <div>
+                <label
+                    htmlFor='Salida:'
+                    className='text-gray-700 uppercase font-bold text-sm'>Fecha de salida: </label>
+                <input
+                    id='salida'
+                    type="date"
+                    className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
+                    placeholder='salida'
+                    name='salida'
+                    value={form.salida}
+                    onChange={handleChange}
+                />
+            </div>
+            <div>
+                <label
+                    htmlFor='sintomas:'
+                    className='text-gray-700 uppercase font-bold text-sm'>Síntomas: </label>
+                <textarea
+                    id='sintomas'
+                    type="text"
+                    className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
+                    placeholder='Ingrese los síntomas de la mascota'
+                    name='sintomas'
+                    value={form.sintomas}
+                    onChange={handleChange}
+                />
+            </div>
 
-      <input
-        type="submit"
-        className="bg-gray-600 w-full p-3 text-slate-300 uppercase font-bold rounded-lg hover:bg-gray-900 cursor-pointer transition-all"
-        value={paciente?._id ? "Actualizar paciente" : "Registrar paciente"}
-      />
-    </form>
-  );
-};
+            <input
+                type="submit"
+                className='bg-gray-600 w-full p-3 
+                    text-slate-300 uppercase font-bold rounded-lg 
+                    hover:bg-gray-900 cursor-pointer transition-all'
+                    value={paciente?._id ? 'Actualizar paciente' : 'Registrar paciente'} />
+
+        </form>
+    )
+}
